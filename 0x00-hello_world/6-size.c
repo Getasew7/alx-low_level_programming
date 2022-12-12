9262d4e263a99e07734f54b718cbd420b19e09db
Getasew7 <getsewendaylalu7721@gmail.com>
2022-12-12 15:14:40 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 
-
 /**
+ *   main - Entry point
  *
- *  * main - prints the size of various types
- *
- *   * Return: 0 if exited properly, non-zero otherwise
- *
+ *   Return: Always 0 (Success)
  */
 
 int main(void)
