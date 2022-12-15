e06acbd0e93455063dc2e4bd9be7d35a89ad8457
Getasew7 <getsewendaylalu7721@gmail.com>
2022-12-15 12:14:20 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,17 @@
 #include "main.h"
+
 /**
- * _isdigit - checks for a digit (0 through 9)
+ * _isupper - checks for uppercase character
  *
- * @c: int to be checked
+ * @c: the character to be checked
  *
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 
-int _isupper(int c);
+int _isupper(int c)
+
 {
-	if (c >= 'A' && c<= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+
+		return (c >= 'A' && c <= 'Z');
 
 }
