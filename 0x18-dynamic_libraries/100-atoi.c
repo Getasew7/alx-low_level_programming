07b87e3ae16c6f9b9b1edb1be896b4d15b1de383
Getasew7 <getasewendaylalu7721@gmail.com>
2023-03-21 00:41:49 +0300

    done

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,25 +1,46 @@
 #include "main.h"
+
 /**
-  *_atoi - converts a string to an integer
-  *@s:is string
-  *Return: returns i* n
-*/
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
 int _atoi(char *s)
 {
-int n = 1;
-unsigned int i = 0;
+	int i, d, n, len, f, digit;
 
-for (; *s; s++)
-{
-if (*s >= '0' && *s <= '9')
-{
-i = i * 10;
-i = i + *s - '0';
-}
-else if (i > 0)
-break;
-else if (*s == '-')
-n = n * -1;
-}
-return (i *n);
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
